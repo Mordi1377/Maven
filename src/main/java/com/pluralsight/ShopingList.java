3a6886ef8ea996862bcc45519b05871ba9b11de0
Mordecai_Anagaw <mordecaianagaw@gmail.com>
2024-09-25 11:10:07 -0700

    completed ShopingList app

---
+++ src/main/java/com/pluralsight/ShopingList.java
@@ -0,0 +1,18 @@
+package com.pluralsight;
+
+import java.net.SocketOption;
+
+public class ShopingList {
+    public static void main(String[] args) {
+        System.out.println("coffe1");
+        System.out.println("coffe2");
+        System.out.println("coffe3");
+        System.out.println("coffe4");
+        System.out.println("coffe5");
+        System.out.println("coffe6");
+        System.out.println("coffe7");
+        System.out.println("coffe8");
+        System.out.println("coffe9");
+        System.out.println("coffe10");
+    }
+}
